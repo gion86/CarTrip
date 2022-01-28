5f526707ea71a4bbbfe0816a649c7a7b192bddb3
Gionata Boccalini <gionata.boccalini@gmail.com>
2022-01-28 19:00:44 +0100

    Added editText and some simple logic to get KM counters.

--- app/src/main/java/com/cartrip/FirstFragment.java
+++ app/src/main/java/com/cartrip/FirstFragment.java
@@ -1,6 +1,8 @@
 package com.cartrip;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,21 @@ import com.cartrip.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+
+    private int startKMCount;
+    private int endKMCount;
+
+    public FirstFragment() {
+    }
+
+    public FirstFragment(int startKM, int endKM) {
+        this.startKMCount = startKM;
+        this.endKMCount = endKM;
+    }
+
+    int getKMCount() {
+        return endKMCount - startKMCount;
+    }
 
     @Override
     public View onCreateView(
@@ -34,6 +51,38 @@ public class FirstFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+
+        binding.startKMCount.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                startKMCount = Integer.parseInt(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        binding.endKMCount.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                endKMCount = Integer.parseInt(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
     }
--- app/src/test/java/com/cartrip/ExampleUnitTest.java
+++ app/src/test/java/com/cartrip/ExampleUnitTest.java
@@ -4,14 +4,18 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import androidx.fragment.app.Fragment;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void kmCountIsPositive() {
+        FirstFragment f1 = new FirstFragment(22, 30);
+        assertTrue(f1.getKMCount() > 0);
     }
 }
