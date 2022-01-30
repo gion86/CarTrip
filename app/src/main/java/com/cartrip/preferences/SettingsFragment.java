24376c76e67e82b51e25a23432b46d9631b7f2ae
Gionata Boccalini <gionata.boccalini@gmail.com>
2022-01-30 18:55:55 +0100

    Added preferences.xml and fragment with navigation.

---
+++ app/src/main/java/com/cartrip/preferences/SettingsFragment.java
@@ -0,0 +1,41 @@
+package com.cartrip.preferences;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.preference.EditTextPreference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import com.cartrip.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        PreferenceManager preferenceManager = getPreferenceManager();
+        preferenceManager.setPreferenceDataStore(EncryptedPreferenceDataStore.getInstance(getContext()));
+
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        EditTextPreference passwordEdit = (EditTextPreference) getPreferenceManager().findPreference("pref_sender_password");
+
+        passwordEdit.setOnBindEditTextListener(editText -> {
+            editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        });
+
+        passwordEdit.setSummaryProvider(preference -> {
+            return "";
+        });
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
