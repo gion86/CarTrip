9a0e6e01d25e6d2c84eeef668b4ff52bf58ebe5e
Gionata Boccalini <gionata.boccalini@gmail.com>
2022-02-01 18:08:31 +0100

    Implemented testFragmentCountersInitState with comparison of the pref value and the ediittext value.

--- app/src/androidTest/java/com/cartrip/KMCountFragmentIT.java
+++ app/src/androidTest/java/com/cartrip/KMCountFragmentIT.java
@@ -1,80 +1,90 @@
 package com.cartrip;
 
+import static android.content.Context.MODE_PRIVATE;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.cartrip.KMCountFragment.PREF_KEY_END_KM;
 import static com.cartrip.KMCountFragment.PREF_KEY_START_KM;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static com.cartrip.KMCountFragment.SHARED_PREF_FILE;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.lifecycle.Lifecycle;
 import androidx.test.filters.MediumTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.MockitoRule;
 
 @MediumTest
 @RunWith(MockitoJUnitRunner.class)
 public class KMCountFragmentIT {
 
-//    @Rule
-//    public MockitoRule rule = MockitoJUnit.rule();
+    private static final int START_KM_COUNT_TEST_VALUE = 10;
+    private static final int END_KM_COUNT_TEST_VALUE = 90;
 
-    @Mock
-    Context context;
+    private SharedPreferences sharedPreferences;
 
-    @Mock
-    SharedPreferences sharedPrefs;
-
-//    @Mock
-//    Fragment fragment;
-
-//    @Mock
-//    SharedPreferences.Editor mockEditor;
-
-//    private AutoCloseable closeable;
+    private int startKMCount;
+    private int endKMCount;
+    private boolean clearPref;
 
     @Before
     public void setUp() {
-//        closeable = MockitoAnnotations.openMocks(this);
-//        fragment = mock(Fragment.class);
-        sharedPrefs = mock(SharedPreferences.class);
-        context = mock(Context.class);
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-//        when(fragment.getContext()).thenReturn(context);
-        when(context.getSharedPreferences(anyString(), anyInt())).thenReturn(sharedPrefs);
+        sharedPreferences = appContext.getSharedPreferences(SHARED_PREF_FILE, MODE_PRIVATE);
+
+        if (sharedPreferences.contains(PREF_KEY_START_KM)) {
+            clearPref = false;
+            startKMCount = sharedPreferences.getInt(PREF_KEY_START_KM, 0);
+        } else {
+            clearPref = true;
+            startKMCount = START_KM_COUNT_TEST_VALUE;
+            SharedPreferences.Editor preferencesEditor = sharedPreferences.edit();
+            preferencesEditor.putInt(PREF_KEY_START_KM, startKMCount);
+            preferencesEditor.apply();
+        }
+
+        if (sharedPreferences.contains(PREF_KEY_END_KM)) {
+            clearPref = false;
+            endKMCount = sharedPreferences.getInt(PREF_KEY_END_KM, 0);
+        } else {
+            clearPref = true;
+            endKMCount = END_KM_COUNT_TEST_VALUE;
+            SharedPreferences.Editor preferencesEditor = sharedPreferences.edit();
+            preferencesEditor.putInt(PREF_KEY_END_KM, endKMCount);
+            preferencesEditor.apply();
+        }
+
+        Log.d("startKMCount = ", String.valueOf(startKMCount));
+        Log.d("endKMCount = ", String.valueOf(endKMCount));
+        Log.d("clearPref = ", String.valueOf(clearPref));
     }
 
     @After
-    public void tearDown() throws Exception {
-//        closeable.close();
+    public void tearDown() {
+        if (clearPref) {
+            sharedPreferences.edit().clear().apply();
+            Log.d("clearPref = ", "CLEARED");
+        }
     }
 
     @Test
-    public void testFragmentCountersInitStata() {
-//        when(sharedPrefs.getInt(eq(PREF_KEY_START_KM), anyInt())).thenReturn(20);
-        when(sharedPrefs.getInt(anyString(), anyInt())).thenReturn(20);
-
+    public void testFragmentCountersInitState() {
         FragmentScenario<KMCountFragment> scenario = FragmentScenario.launchInContainer(KMCountFragment.class);
         scenario.moveToState(Lifecycle.State.RESUMED);
 
-        onView(withId(R.id.startKMCount)).check(matches(withText("20")));
+        onView(withId(R.id.startKMCount)).check(matches(withText(String.valueOf(startKMCount))));
+        onView(withId(R.id.endKMCount)).check(matches(withText(String.valueOf(endKMCount))));
     }
 }
